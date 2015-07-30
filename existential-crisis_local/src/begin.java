1cd216533654ec4891ea37aa8856530fbc3aca5c
Brian <brian.mayers90@gmail.com>
2015-07-30 18:21:45 +0100

    Initial checkin of stub code

---
+++ existential-crisis_local/src/begin.java
@@ -0,0 +1,16 @@
+
+public class begin {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO stuff and junk
+		printGreeting();
+	}
+	
+	private static void printGreeting(){
+		System.out.println("Let's begin");
+	}
+
+}
